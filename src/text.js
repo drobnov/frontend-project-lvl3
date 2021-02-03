9e04583f885681e7b99f2144e0a403963f775aba
Pavlo Drobnov <drobnovpavlo89@gmail.com>
2021-02-03 21:40:38 +0200

    make text interface

---
+++ src/text.js
@@ -0,0 +1,18 @@
+// @ts-check
+
+export default {
+  errors: {
+    inValidUrl: 'Must be valid url',
+    inValidRss: "This source doesn't contain valid rss",
+    rssExists: 'Rss already exists',
+    networkError: 'Network error',
+  },
+  success: 'Rss has been loaded',
+  h2: {
+    feeds: 'Feeds',
+    posts: 'Posts',
+  },
+  button: {
+    preview: 'Preview',
+  },
+};
