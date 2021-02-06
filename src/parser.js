b853ad033492805112b6557559043dad4cdb52bd
Pavlo Drobnov <drobnovpavlo89@gmail.com>
2021-02-06 13:38:24 +0200

    refactoring

--- src/parser.js
+++ src/parser.js
@@ -7,19 +7,20 @@ const parserDom = (rss) => {
   return doc.querySelector('rss');
 };
 
-const parserFeeds = (rss) => {
+const parserFeeds = (rss, rssLink) => {
   const title = rss.querySelector('channel > title');
   const description = rss.querySelector('channel > description');
   const link = rss.querySelector('channel > link');
   const result = {
-    title: title.textContent,
-    description: description.textContent,
+    title: title.childNodes[0].textContent,
+    description: description.childNodes[0].textContent,
     link: link.nextSibling.textContent,
+    rssLink,
   };
   return result;
 };
 
-const parsePosts = (rss, parentsLink) => {
+const parsePosts = (rss, parentsLink, rssLink) => {
   const posts = rss.querySelectorAll('item');
   const result = Array.from(posts).map((item) => {
     const title = item.querySelector('title');
@@ -28,22 +29,23 @@ const parsePosts = (rss, parentsLink) => {
     const id = uniqueId();
     return ({
       parentsLink,
-      title: title.textContent,
-      description: description.textContent,
+      title: title.childNodes[0].textContent,
+      description: description.childNodes[0].textContent,
       link: link.nextSibling.textContent,
       id,
+      rssLink,
     });
   });
   return result;
 };
 
-const parseRss = (element) => {
+const parseRss = (element, rssLink) => {
   const elementRss = parserDom(element);
   if (elementRss === null) {
     return null;
   }
-  const feeds = parserFeeds(elementRss);
-  const posts = parsePosts(elementRss, feeds.link);
+  const feeds = parserFeeds(elementRss, rssLink);
+  const posts = parsePosts(elementRss, feeds.link, rssLink);
   return { feeds, posts };
 };
 
