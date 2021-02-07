14c27f299ca7d7b5fc91e89e7876c176be4c4b09
Pavlo Drobnov <drobnovpavlo89@gmail.com>
2021-02-07 16:30:34 +0200

    refactor

--- src/init.js
+++ src/init.js
@@ -151,7 +151,20 @@ export default () => {
     resources: {
       en: {
         translation: {
-          ...text,
+          errors: {
+            inValidUrl: 'Must be valid url',
+            inValidRss: "This source doesn't contain valid rss",
+            rssExists: 'Rss already exists',
+            networkError: 'Network error',
+          },
+          success: 'Rss has been loaded',
+          h2: {
+            feeds: 'Feeds',
+            posts: 'Posts',
+          },
+          button: {
+            preview: 'Preview',
+          },
         },
       },
     },
